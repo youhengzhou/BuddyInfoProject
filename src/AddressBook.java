12fec1a381fe4dbacabe89c0a4cdc33f79968d19
youhengzhou <youhengzhou@cmail.carleton.ca>
2020-09-25 09:48:30 -0400

    I added more methods to AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,22 @@
+import java.util.*;
 public class AddressBook {
+    private List<BuddyInfo> buddyList;
+    public AddressBook(){
+        buddyList = new ArrayList<>();
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Address Book");
+    public void addBuddy(BuddyInfo buddy){
+        this.buddyList.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        this.buddyList.remove(buddy);
+    }
+
+    public static void main(String[] args){
+        BuddyInfo buddy = new BuddyInfo("Tom", "carleton");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
